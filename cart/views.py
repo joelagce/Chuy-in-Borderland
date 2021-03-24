e251230807b51dbc5cfe86cb38726c737472c834
MartinezAnak <79723613+MartinezAnak@users.noreply.github.com>
2021-03-24 09:20:26 -0700

    Update views.py

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+from .cart import Cart 
+
+
+
+def cart_detail(request):
+    cart = Cart(request)
+    productsstring = ''
+
+    for item in cart:
+        product = item['product']
+        url = '/%s/%s/' % (product.category.slug, product.slug)
+        b = "{'id': '%s', 'title': '%s', 'price': '%s', 'quantity': '%s', 'total_price': '%s', 'thumbnail': '%s', 'url': '%s','num_available': '%s'}," % (product.id, product.title, product.price, item['quantity'], item['total_price'],  product.get_thumbnail, url, product.num_available)"
+
+        productsstring = productsstring + b
+
+    context = {
+        'cart': cart,
+        'pub_key': settings.STRIPE_API_KEY_PUBLISHABLE,
+        'pub_key_paypal':settings.PAYPAL_API_KEY_PUBLISHABLE,
+        'productsstring': productsstring.rstrip(',')
+    }
+
+    return render(request, 'cart.html', context)
+
+
+def succes(request):
+    cart = Cart(request)
+    cart.clear()
+    return render(request, 'succes.html')
+    
+
+
